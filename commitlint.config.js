09f4161679e5081569b4d675d9ffed884d5715c1
tam5 <arimiller92@gmail.com>
2022-11-30 08:40:28 -0500

    try shared commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules:{
+    'subject-case': [0, 'never'],
+    'scope-case': [2, 'always', ['upper-case', 'lower-case']],
+  },
+  ignores: [
+    (message) => message.includes('WIP'),
+    (message) => message.includes('Initial commit'),
+  ]
+};
